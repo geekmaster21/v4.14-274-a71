12a157fb688b44ff4ec6bcec3e952b0e1ca2eafc
geekmaster21 <maya-10@hotmail.ca>
2022-04-20 19:34:07 +0000

    Add Samsung A52 Defconfig

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,13 +2,19 @@
 
 export ARCH=arm64
 mkdir out
-
+DEF1=sm7150_sec_defconfig
+DEF2=sm7150_sec_eng_defconfig
+DEF3=sm7150_sec_a71_eur_open_defconfig
 BUILD_CROSS_COMPILE=/home/ubuntu/android_tools/kernels/msm-4.14/toolchain/gcc/bin/aarch64-linux-android-
 KERNEL_LLVM_BIN=/home/ubuntu/android_tools/kernels/msm-4.14/toolchain/clang/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE a71_eur_open_defconfig
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE \
+sm7150_sec_a71_eur_open_defconfig \
+sm7150_sec_defconfig \
+a71_eur_open_defconfig
+
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
  
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
